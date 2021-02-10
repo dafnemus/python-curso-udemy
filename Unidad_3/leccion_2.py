726d0f56a9af75f370325aa41c99fd2374ef40b5
dafnemus <dafnemus@gmail.com>
2021-02-10 17:13:01 -0300

    distingo el tipo de caracter simbolo/letra/digito

--- Unidad_3/leccion_2.py
+++ Unidad_3/leccion_2.py
@@ -6,3 +6,13 @@ if numero % 2 == 0:
     print(f'Es par {numero}')
 else:
     print(f'Es impar {numero}')
+
+# determinar si es letra, digito o simbolo
+caracter = input('ingrese un caracter: ')
+
+if caracter.isalpha():
+    print(f'{caracter} es letra')
+elif caracter.isdigit():
+    print(f'{caracter} es un digito')
+else:
+    print(f'{caracter} es un simbolo')
