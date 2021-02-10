9d6653cd80f22960b4b0fb9a7ce52683df2d6728
dafnemus <dafnemus@gmail.com>
2021-02-10 16:28:09 -0300

    convierto horas a segundos

--- Unidad_3/leccion_1.py
+++ Unidad_3/leccion_1.py
@@ -4,3 +4,8 @@ print(f'Hola {nombre}')  # formato 1
 
 print('Hola', end=' ')  # formato 2
 print(nombre)
+
+# conversión de horas a segundos.
+horas = int(input('Ingrese cantidad de Horas: '))
+UN_SEGUNDO = 3600
+segundos_totales = horas * UN_SEGUNDO
