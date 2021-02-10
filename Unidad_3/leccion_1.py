8a03ccc9714a91aad5bea8eee49929d75f60fa97
dafnemus <dafnemus@gmail.com>
2021-02-10 16:39:06 -0300

    convierto celcius a farenheit

--- Unidad_3/leccion_1.py
+++ Unidad_3/leccion_1.py
@@ -9,3 +9,12 @@ print(nombre)
 horas = int(input('Ingrese cantidad de Horas: '))
 UN_SEGUNDO = 3600
 segundos_totales = horas * UN_SEGUNDO
+print(f'segundos {segundos_totales}')
+
+# conversion de Celsius a Farenheit.
+# formula (0 °C × 9/5) + 32 = 32 °F
+
+celsius = int(input('Ingrese cantidad de grados Celsius: '))
+VALOR_FARENHEIT = 32
+farenheits = (celsius * 9/5) + VALOR_FARENHEIT
+print(f'{farenheits}° F')
