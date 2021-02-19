d07c1e7cfe85af4b4d98cdd94e073926ef781b29
dafnemus <dafnemus@gmail.com>
2021-02-19 17:55:00 -0300

    agrego pistas a mis funciones

--- Unidad_8/funciones_1.py
+++ Unidad_8/funciones_1.py
@@ -3,11 +3,8 @@
 # Crear una funcion que diga: Hola y salude a una persona
 
 
-def saludar(name=''):
+def saludar(name: str) -> str:
     print('Hola', name)
-
-
-saludar()
 
 
 saludar('marta')
@@ -15,7 +12,7 @@ saludar('marta')
 vocales = {}
 
 
-def contar_vocales(nombre):
+def contar_vocales(nombre: str) -> dict:
     nombre = nombre.lower()
 
     for vocal in 'aeiou':
@@ -32,3 +29,19 @@ nombres = ['luis', 'pablo', 'lola', 'julieta']
 for persona in nombres:
     dict_vocal = contar_vocales(persona)
     print(f'vocales {persona}: {dict_vocal}')
+
+# Funcion Par usando pistas:
+
+
+def es_par(numero: int) -> bool:
+    if numero % 2 == 0:
+        return True
+    return False
+
+
+es_par(2)
+
+# seber si es par dentro de un rango:
+
+for num in range(2, 10):
+    print(num, es_par(num))
