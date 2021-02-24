b611e163c43070ed2c37c9d9ed15dfbdd83a6ac4
dafnemus <dafnemus@gmail.com>
2021-02-24 13:50:17 -0300

    mini app con PyQT5

---
+++ PyQT_Tkinter/ej_pyqt.py
@@ -0,0 +1,23 @@
+# pylint: disable=missing-docstring
+
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+
+# creo una instancia de QApplication
+app = QApplication(sys.argv)
+
+# instancia GUI de la aplicacion:
+# mini app
+ventana = QWidget()
+ventana.setWindowTitle('App PyQt5, by Dafne')
+ventana.setGeometry(100, 100, 480, 80)
+ventana.move(60, 15)
+
+mensaje = QLabel('<h1>Hola, esto es Python', parent=ventana)
+mensaje.move(60, 15)
+
+# App
+ventana.show()
+
+# Aplicacion corriendo:
+sys.exit(app.exec_())
