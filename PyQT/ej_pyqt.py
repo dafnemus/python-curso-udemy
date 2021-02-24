53ec230c633a21bd0de2488f87443b898be87ad7
dafnemus <dafnemus@gmail.com>
2021-02-24 14:20:43 -0300

    actualizo el archivo requeriments.txt

--- PyQT/ej_pyqt.py
+++ PyQT/ej_pyqt.py
@@ -6,13 +6,14 @@ from PyQt5.QtWidgets import QApplication, QLabel, QWidget
 # creo una instancia de QApplication
 app = QApplication(sys.argv)
 
-# instancia GUI de la aplicacion:
+# instancia principal GUI de la aplicacion:
 # mini app
 ventana = QWidget()
 ventana.setWindowTitle('App PyQt5, by Dafne')
 ventana.setGeometry(100, 100, 480, 80)
 ventana.move(60, 15)
 
+# contenido de la ventana.
 mensaje = QLabel('<h1>Hola, esto es Python', parent=ventana)
 mensaje.move(60, 15)
 
